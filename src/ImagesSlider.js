850227b3837283c9ea6954f934ccb9c9ff0c8408
HakobyanStepan <stepanhakobyan89@gmail.com>
2022-07-03 01:25:51 +0300

    first commit

---
+++ src/ImagesSlider.js
@@ -0,0 +1,40 @@
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./style.css"
+import React from 'react'
+
+  
+const ImagesSlider = ({images}) => {
+  
+  const settings = {
+    infinite: true,
+    dots: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: true,
+    autoplay: true,
+  autoplaySpeed: 3000,
+  
+  };
+  
+  return (
+    <>
+    <div className="tag">
+          <h1>Carousel</h1>
+    </div>
+      <div className="imgslider">
+        <Slider {...settings}>
+          {images.map((item) => (
+            <div key={item.id}>
+              <img src={require(`${item.src}`)}  alt={item.alt} />
+              
+              
+            </div>
+          ))}
+        </Slider>
+      </div>
+          </>
+  )
+}
+export default ImagesSlider;
---
+++ src/images.js
@@ -0,0 +1,18 @@
+const images = [{
+    id: 1,
+   src:"./Image/1.jpg",
+    alt: "Image 1"
+},
+{
+    id: 2,
+    src: "./Image/2.jpg",
+    alt: "Image 2 "
+},
+
+{
+    id: 3,
+    src: "./Image/3.jpg",
+    alt: "Image 3"
+}
+];
+export default images;
